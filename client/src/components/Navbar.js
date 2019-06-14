ad1619566eb2d7314d3d3a848cf4b9e37317bdbf
Amanda Garcia <algarcian@outlook.com>
2019-06-14 11:50:56 -0600

    Navbar comp and images

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom';
+import {Menu, Segment, Image, } from 'semantic-ui-react'; 
+// import styled from 'styled-components';
 
 
 const Navbar = () => (
   <>
-    <h1>Navbar</h1>
+  <Segment inverted>
+    <Menu inverted pointing secondary>
+      <Menu.Item>
+        <NavLink exact path to="/" activeStyle={styles.active}>Home</NavLink>
+      </Menu.Item>
+      <Menu.Item>
+        <NavLink exact path to="/about" activeStyle={styles.active}>About</NavLink>
+      </Menu.Item>
+      <Menu.Item>
+        <NavLink exact path to="/departments" activeStyle={styles.active}>Departments</NavLink>
+      </Menu.Item>
+    </Menu>
+  </Segment>
   </>
 )
+
+const styles = {
+  active: {
+    color: 'orange', 
+    fontWeight: 'bold', 
+  }
+}
 
 export default Navbar; 
